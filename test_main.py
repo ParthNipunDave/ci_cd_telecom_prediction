70dd740f32282f09cd3c9402f2ce7c593cb762e8
Parth Nipun Dave <dave.n.parth@gmail.com>
2024-02-10 21:02:10 +0530

    pushing testing back

--- test_main.py
+++ test_main.py
@@ -21,8 +21,4 @@ def test_predict_route(client):
     assert response.status_code == 200
 
 
-def test_prediction(client):
-    """Test the prediction functionality."""
-    response = client.get('/predict?tenure=1&security=1&tech=1&contract=1&charges=1')
-    assert response.status_code == 200
-    assert b"Churn" in response.data
+
